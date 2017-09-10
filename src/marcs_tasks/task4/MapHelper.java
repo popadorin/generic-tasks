c4219d9226c5208eb73a228beb294e3c028f12a8
dorin95dp <dorin.popa1995@gmail.com>
2017-09-10 14:32:44 +0300

    Implement MapHelper from task4

--- src/marcs_tasks/task4/MapHelper.java
+++ src/marcs_tasks/task4/MapHelper.java
@@ -1,23 +1,22 @@
 package marcs_tasks.task4;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class MapHelper {
-	
-	
-	public <T> Map<T, Integer> getValuesAndOccurences(T ...args) {
-		for (T t : args) {
-			
-		}
-		
-		return null;
-	}
 
-	private <T> int getNrOfOccurences(T t, T[] array) {
-		
-		
-		return 0;
-		
-	}
+    public <T> Map<T, Integer> getValuesAndOccurences(T ...args) {
+        HashMap<T, Integer> valuesAndOccurences = new HashMap<>(); // dict with word name key and frequency value
 
+        for (T arg : args) {
+            if (valuesAndOccurences.containsKey(arg)) {
+                valuesAndOccurences.put(arg, valuesAndOccurences.get(arg) + 1);
+            } else {
+                valuesAndOccurences.put(arg, 1);
+            }
+        }
+
+        return valuesAndOccurences;
+    }
+	
 }
